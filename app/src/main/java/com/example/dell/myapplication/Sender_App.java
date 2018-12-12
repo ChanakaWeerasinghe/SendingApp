7b0c2713c0de188e03c793342f721dccec30908f
chanaka weerasinghe <chanaka@payable.lk>
2018-12-12 11:37:05 +0530

    sending broadcasts and intent to other app

--- app/src/main/java/com/example/dell/myapplication/Sender_App.java
+++ app/src/main/java/com/example/dell/myapplication/Sender_App.java
@@ -1,9 +1,12 @@
 package com.example.dell.myapplication;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,26 +17,43 @@ public class Sender_App extends AppCompatActivity {
     private static final int SELECT_PICTURE = 1;
     private String selectedImagePath;
     private ImageView img;
+    private LocalBroadcastManager broadcaster;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sender__app);
-
+        broadcaster = LocalBroadcastManager.getInstance(this);
         img = (ImageView) findViewById(R.id.imageView1);
         Button send = (Button) findViewById(R.id.button1);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT,
-                        "This is my text to send.");
-                sendIntent.putExtra("MyKey",
-                        "This is second my text to send using my key.");
-                sendIntent.setType("text/plain");
-//                startActivity(Intent.createChooser(sendIntent, "Send"));
-                startActivity(sendIntent);
+// send intent to other activity
+//                Intent sendIntent = new Intent();
+//                sendIntent.setAction(Intent.ACTION_SEND);
+//                sendIntent.putExtra(Intent.EXTRA_TEXT,
+//                        "This is my text to send.");
+//                sendIntent.putExtra("MyKey",
+//                        "This is second my text to send using my key.");
+//                sendIntent.setType("text/plain");
+////                startActivity(Intent.createChooser(sendIntent, "Send"));
+//                startActivity(sendIntent);
+
+
+//                broadcast with sendBroadcast
+                final Intent intent=new Intent();
+                intent.setAction("com.pkg.perform.Ruby");
+                intent.putExtra("KeyName","code1id");
+                intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+                intent.setComponent(
+                        new ComponentName("com.example.dell.resiving","com.example.dell.resiving.MyBroadcastReceiver"));
+                sendBroadcast(intent);
+
+
+
+
             }
         });
 
